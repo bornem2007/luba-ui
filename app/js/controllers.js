60154ed3b457ffdf69a02a6326f87648ae2679c2
Csaba_Bornemissza <bornem2007@yahoo.co.uk>
2014-05-08 17:44:33 +0200

    Added register user feature

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,6 +20,10 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$h
     });
   }]);
 
+/**
+ * This method handles the register button click.
+ * Validates input, and sends the register user backend call, dispays results.
+ */
 phonecatControllers.controller('RegisterCtrl', ['$scope', '$routeParams', '$http',
     function($scope, $routeParams, $http) {
 
@@ -27,18 +31,43 @@ phonecatControllers.controller('RegisterCtrl', ['$scope', '$routeParams', '$http
         $scope.txtLogin = $scope.txtLoginDefaultMessage;
         $scope.txtPass1 = "";
         $scope.txtPass2 = "";
+        $scope.msgLogin = "";
+        $scope.msgPass1 = "";
+        $scope.msgPass2 = "";
+        $scope.msgStatus = "";
 
         $scope.register = function(){
+            $scope.msgLogin = "";
+            $scope.msgPass1 = "";
+            $scope.msgPass2 = "";
+            $scope.msgStatus = "";
             console.log('register clicked, login='+$scope.txtLogin+', pass1='+$scope.txtPass1+' , pass2='+$scope.txtPass2);
+            console.log('register $scope.txtLogin.length=' + $scope.txtLogin.length);
 
-            //if $scope
+            if ($scope.txtLogin.length <4) {
+                $scope.msgLogin = "Error: Login name must be at least 6 characters!";
+                return;
+            }
+
+            if ($scope.txtPass1 != $scope.txtPass2) {
+                $scope.msgPass2 = "Error: Passwords must match!";
+                return;
+            }
 
             $http({method: 'GET', url: 'http://localhost:3000/createuser?login='+$scope.txtLogin + '&password='+$scope.txtPass1}).
-                success(function(data, status, headers, config) {
+                success(function(data) {
                     console.log('success: data:' + JSON.stringify( data ));
+                    if (data.code == "OK"){
+                        $scope.msgStatus = "You have been registered successfully!"
+                    }else
+                    {
+                        $scope.msgStatus = "Error registering:"+data.code;
+                    }
+
                 }).
-                error(function(data, status, headers, config) {
+                error(function(data) {
                     console.log('error: data:' + JSON.stringify( data ));
+                    $scope.msgStatus = "Unknown Error during registering:" + JSON.stringify(data);
                 });
         }
 
